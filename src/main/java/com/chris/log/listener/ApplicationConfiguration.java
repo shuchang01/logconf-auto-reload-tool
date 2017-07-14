6fef61bbae6b76263ab9b9999021dd5615260f6f
Chris <chris.shang@qq.com>
2017-07-14 15:59:37 +0800

    update ApplicationConfiguration class, use another method of singlqq

--- src/main/java/com/chris/log/listener/ApplicationConfiguration.java
+++ src/main/java/com/chris/log/listener/ApplicationConfiguration.java
@@ -8,25 +8,53 @@ import java.util.Properties;
 
 /**
  * <p>
+ * 基于jdk1.7+的修改properties配置文件热部署实现方案
+ * <p>
+ * 该类是单例的，《Java并发编程实践》一书建议用静态内部类单例模式来替代DCL (双重检查模式 : Double Check Pattern)。
+ * <p>
  * 参考博文
  * http://howtodoinjava.com/java-7/auto-reload-of-configuration-when-any-change-happen/
- * <p>
- * 基于jdk1.7+的修改properties配置文件热部署实现方案
  * 
  * @author anonymous author
  * @author Chris
  * @since July 11, 2017
  */
 public class ApplicationConfiguration {
-	private static final ApplicationConfiguration ac = new ApplicationConfiguration();
-
+	// 第一种传统写法，DCL
+//	private static final ApplicationConfiguration ac = new ApplicationConfiguration();
+//	private ApplicationConfiguration() {
+//	}
+//	public static ApplicationConfiguration getInstance() {
+//		return ac;
+//	}
+	
+	// 第二种支持多线程，线程安全的写法
 	private ApplicationConfiguration() {
 	}
-
 	public static ApplicationConfiguration getInstance() {
-		return ac;
+		return SingletonHolder.getInstance2();
 	}
 	
+	/**
+	 * 静态内部类，private access
+	 * @author shangpan
+	 */
+	private static class SingletonHolder {
+//		private static final Singleton2 sInstance = new Singleton2(); // 传统写法
+		private static ApplicationConfiguration sInstance = getInstance2(); 
+		
+		private static ApplicationConfiguration getInstance2() {
+			if (sInstance == null) {
+				synchronized (ApplicationConfiguration.class) {
+					if (sInstance == null) {
+						sInstance = new ApplicationConfiguration();
+					}
+				}
+			}
+			return sInstance;
+		}
+	}
+/***************************************************************************************/ 	
 	private static Properties configuration = new Properties();
 
 	private static Properties getConfiguration() {
